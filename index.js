58735d27169d57dbeacee36068f67202fc3ee767
huanghao <hao.huang04@hand-china.com>
2019-05-17 14:32:12 +0800

    修改3

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var fs = require("fs");
 var _ = require('lodash');
 
 //workbook 对象，指的是整份 Excel 文档。我们在使用 js-xlsx 读取 Excel 文档之后就会获得 workbook 对象。
-var workbook =  xl.readFile("./TIME日记产品表5.6总表的副本.xlsx")
+var workbook =  xl.readFile("./data/TIME日记产品表5.6总表的副本.xlsx")
 
 
 
